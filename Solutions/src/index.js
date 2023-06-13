c51aa909fc40e32b14b77827affb407045f7f1d5
Gabhan <gabhan@gabhan.com>
2023-06-13 19:34:09 +0100

    nice cleanups

--- Solutions/src/index.js
+++ Solutions/src/index.js
@@ -6,54 +6,49 @@ import { createRoot } from 'react-dom/client';
 
 import Gabhan from './images/Gabhan.jpg';
 
-const title = <h1>Getting Started React</h1>
-const welcome = 'Welcome to 30 Days Of React'
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
+const Header = (props) => {
+  console.log(props);
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>{props.welcome}</h1>
+        <h2>{props.title}</h2>
+        <h3>{props.subtitle}</h3>
+        <small>{props.date}</small>
+      </div>
+    </header>
+  )
+}
+
+const UserCard = (props) => {
+  const yearBorn = 1972
+  const currentYear = 2023
+  const age = currentYear - yearBorn  
+  let status = age >= 18
+
+  console.log(props);
+
+  return (
+    <div className='user-card'>
+      <img src={Gabhan} alt='' />
+      <h2>{props.firstName}</h2>
+      <Age age={age} />
+      <Status status={status} />
     </div>
-  </header>
-)
+  )
+}
 
-const yearBorn = 1972
-const currentYear = 2023
-const age = currentYear - yearBorn
-const myAge = (
-  <p>
-    {' '}
-    Gabhan is {age} years old.
-  </p>
-)
-
-const UserCard = () => (
-  <div className='user-card'>
-    <img src={Gabhan} alt='gabhan' />
-    <h2>Gabhan</h2>
-  </div>
-)
-
-const TechList = () => {
-  const techs = ['HTML', 'CSS', 'JavaScript', 'c#', 'SQL']
-  const techsFormatted = techs.map((tech) => <li>{tech}</li>)
-  return techsFormatted
+const TechList = (props) => {
+  const techsFormatted = props.skills.map((tech) => <li key={tech}>{tech}</li>)
+  return <ul>{techsFormatted}</ul>
 }
 // JSX element, main
 const Main = () => (
   <main>
     <div className='main-wrapper'>
       <p>Prerequisite to get started react.js:</p>
-      <ul>
-        <TechList />
-      </ul>
-      {myAge}
-      <UserCard />    
-      <div>
-      <HexaColor />
-      <HexaColor />
-      </div>
+      <TechList skills={['HTML', 'CSS', 'C#', 'React']} />
+      <UserCard firstName='Gabhan' />    
     </div>
   </main>
 )
@@ -67,46 +62,38 @@ const Footer = () => (
   </footer>
 )
 
-const hexaColor = () => {
-  let str = '0123456789abcdef';
-  let color = '' 
-  for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
-  }
-  return '#' + color
-}
-
-
-const HexaColor = () => {
-  const bgColor = hexaColor()
-  const styles = {
-    height: '100px',
-    display: 'flex',
-    'justify-content': 'center',
-    'align-items': 'center',
-    fontFamily: 'Montserrat',
-    margin: '2px auto',
-    borderRadius: '5px',
-    width: '75%',
-    border: '2px solid black',
-    background: `${bgColor}`
-  }
+const Age = (props) => {
   return (
-    <div style={styles}>
-      <h2>{bgColor}</h2>
-    </div>
+  <div>The person is {props.age} years old.</div>
   )
 }
 
+const Status = (props) => {
+  // ternary operator to check the status of the person
+  let status = props.status ? 'Old enough to drive' : 'Too young for driving'
+  return <p>{status}</p>
+}
+
 // JSX element, app, a container or a parent
-const App = () => (
+const App = () => {
+  const welcome='Welcome to 30 Days Of React' 
+  const title='Getting Started'
+  const subtitle='Every Day is a Learning Day!'
+  const date='13th June 2023'   
+
+return (
   <div className='app'>   
-   <Header />
+    <Header 
+      welcome={welcome}
+      title={title}
+      subtitle={subtitle}
+      date={date}         
+      />    
     <Main />
     <Footer />
   </div>
 )
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
