aca71387cba4f9b45ba32d541e0d0087b670ba24
Gabhan <gabhan@gabhan.com>
2023-06-14 20:06:21 +0100

    Basic mapping

--- Solutions/src/index.js
+++ Solutions/src/index.js
@@ -4,141 +4,20 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import Gabhan from './images/Gabhan.jpg';
-
-const showDate = (time) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-
-  const month = months[time.getMonth()].slice(0, 3)
-  const year = time.getFullYear()
-  const date = time.getDate()
-  return ` ${month} ${date}, ${year}`
+const Numbers = ({numbers}) => {
+  const list = numbers.map((number) => <li>{number}</li>)
+  return list
 }
-
-const Header = ({
-  data: {
-    welcome, 
-    title, 
-    subtitle, 
-    author: {firstName, lastName}, 
-    date}
-  }) => {
-  return (
-    <header>
-      <div className='header-wrapper'>
-        <h1>{welcome}</h1>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>
-          {firstName} {lastName}
-        </p>
-        <small>{showDate(date)}</small>
-      </div>
-    </header>
-  )
-}
-
-const TechList = (props) => {
-  const techsFormatted = props.skills.map((tech) => <li key={tech}>{tech}</li>)
-  return <ul>{techsFormatted}</ul>
-}
-
-const UserCard = ({ user: { firstName, lastName, image } }) => (
-  <div className='user-card'>
-    <img src={image} alt={firstName} />
-    <h2>
-      {firstName}
-      {lastName}
-    </h2>
-  </div>
-)
-
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-)
-
-const buttonStyles = {
-  backgroundColor: '#61dbfb',
-  padding: 10,
-  border: 'none',
-  borderRadius: 5,
-  margin: 3,
-  cursor: 'pointer',
-  fontSize: 18,
-  color: 'white',
-}
-
-// JSX element, main
-const Main = ({ user, techs, greetPeople, handleTime }) => (
-  <main>
-    <div className='main-wrapper'>
-      <p>Prerequisite to get started react.js:</p>
-      <TechList skills={techs} />
-      <UserCard user={user} />
-      <Button text='Greet People' onClick={greetPeople} style={buttonStyles} />
-      <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
-    </div>
-  </main>
-)
-
-// JSX element, footer
-const Footer = ({ copyRight }) => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>Copyright {copyRight.getFullYear()}</p>
-    </div>
-  </footer>
-)
-
-// JSX element, app, a container or a parent
 const App = () => {
-  const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Getting Started React',
-    subtitle: 'JavaScript Library',
-    author: {
-      firstName: 'Gabhan',
-      lastName: 'OLoughlin',
-    },
-    date: new Date(), // date needs to be formatted to a human readable format
-  }
-
-  const techs = ['HTML', 'CSS', 'JavaScript']
-  // copying the author from data object to user variable using spread operator
-  const user = { ...data.author, image: Gabhan }
-
-  const handleTime = () => {
-    alert(showDate(new Date()))
-  }
-  const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2020')
-  }
-
+  const numbers = [1, 2, 3, 4, 5]
   return (
-    <div className='app'>
-      <Header data={data} />
-      <Main
-        user={user}
-        techs={techs}
-        handleTime={handleTime}
-        greetPeople={greetPeople}
-      />
-      <Footer copyRight={data.date} />
+    <div className='container'>
+      <div>
+        <h1>Numbers List</h1>        
+        <ul>
+        <Numbers numbers={numbers} />
+        </ul>
+      </div>
     </div>
   )
 }
