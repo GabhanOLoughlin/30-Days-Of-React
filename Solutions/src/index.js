02db28cf08c214822eae70afb6bf43475875b8e7
Gabhan <gabhan@gabhan.com>
2023-06-14 20:09:20 +0100

    Mapping Array of Arrays

--- Solutions/src/index.js
+++ Solutions/src/index.js
@@ -8,6 +8,29 @@ const Numbers = ({numbers}) => {
   const list = numbers.map((number) => <li>{number}</li>)
   return list
 }
+
+const skills = [
+  ['HTML', 10],
+  ['CSS', 7],
+  ['JavaScript', 9],
+  ['React', 8],
+]
+
+// Skill Component
+const Skill = ({ skill: [tech, level] }) => (
+  <li>
+    {tech} {level}
+  </li>
+)
+
+
+// Skills Component
+const Skills = ({ skills }) => {
+  const skillsList = skills.map((skill) => <Skill skill={skill} />)
+  console.log(skillsList)
+  return <ul>{skillsList}</ul>
+}
+
 const App = () => {
   const numbers = [1, 2, 3, 4, 5]
   return (
@@ -17,6 +40,10 @@ const App = () => {
         <ul>
         <Numbers numbers={numbers} />
         </ul>
+      </div>
+      <div>
+        <h1>Skills Level</h1>
+        <Skills skills={skills} />
       </div>
     </div>
   )
