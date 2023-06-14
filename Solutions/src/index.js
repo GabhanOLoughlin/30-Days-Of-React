bcd3ef1513735786efc50e2a57b23f5c98d96c4d
Gabhan <gabhan@gabhan.com>
2023-06-14 15:08:08 +0100

    Desctucturing props 2

--- Solutions/src/index.js
+++ Solutions/src/index.js
@@ -30,8 +30,7 @@ const showDate = (time) => {
 
 const Header = (props) => {
   const data = props.data;
-  const {welcome, title, subtitle, author, date } = data;
-  const {firstName, lastName} = author;
+  const {welcome, title, subtitle, author: {firstName, lastName}, date } = data;
   return (
     <header>
       <div className='header-wrapper'>
