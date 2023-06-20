ec4314df42e34bac52b50f981c392b4be02b9a38
Gabhan <gabhan@gabhan.com>
2023-06-20 18:16:22 +0100

    Component Life Cycle P1

--- Solutions/src/App.js
+++ Solutions/src/App.js
@@ -1,301 +1,34 @@
 import './App.css';
 
-import { useState } from 'react';
+import React, { Component } from 'react';
 
-const countries = [
-  {
-    value: '',
-    label: '-- Select Country--',
-  },
-  {
-    value: 'Finland',
-    label: 'Finland',
-  },
-  {
-    value: 'Sweden',
-    label: 'Sweden',
-  },
-  {
-    value: 'Norway',
-    label: 'Norway',
-  },
-  {
-    value: 'Denmark',
-    label: 'Denmark',
-  },
-]
-
-const selectCountries = countries.map(({ value, label }) => (
-  <option value={value}> {label}</option>
-))
-
-function App() { 
-  const [state, setState] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    country: '',
-    title: '',
-    tel: '',
-    dateOfBirth: '',
-    favoriteColor: null,
-    weight: '',
-    gender: '',
-    file: '',
-    bio: '',
-    skills: {
-      html: false,
-      css: false,
-      javascript: false,
-    },
-    touched: {
-      firstName: false,
-      lastName: false,
-    },
-  });
-
-  const handleChange = (e) => {    
-    const {name, value, type, checked} = e.target;
-    console.log(e.target);
-    if (type === 'checkbox') {
-      setState({...state,
-        skills: {...state.skills, [name]: checked}});
-    } else if (type === 'file') {
-      setState({...state,
-        [name]: e.target.files[0]});
-    } else {
-      setState({...state,
-        [name]: value});
-    }
-  }
-
-  const handleBlur = (e) => {
-    const { name, value } = e.target
-    setState({ ...state, touched: { ...state.touched, [name]: true } })
-  }
-
-  const validate = () => {
-    // Object to collect error feedback and to display on the form
-    const errors = {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('I am  the constructor and  I will be the first to run.')
+    this.state = {
       firstName: '',
     }
-
-    if (
-      (state.touched.firstName && state.firstName.length < 3) ||
-      (state.touched.firstName && state.firstName.length > 12)
-    ) {
-      errors.firstName = 'First name must be between 2 and 12'
-    }
-    return errors
   }
 
-  const handleSubmit = (e) => {
-    /* 
-     e.preventDefault()
-      stops the default behavior of form element
-     specifically refreshing of page
-     */
-    e.preventDefault()
-
-    /*
-     the is the place where we connect backend api 
-     to send the data to the database
-     */
-    console.log(this.title.current.value);
-
-    console.log(state)
+  static getDerivedStateFromProps(props, state) {
+    console.log(
+      'I am getDerivedStateFromProps and I will be the second to run.'
+    )
+    return null
+  }
+  componentDidMount() {
+    console.log('I am componentDidMount and I will be last to run.')
   }
 
-  const { firstName } = validate()
-
-  return (
-    <div className='App'>      
-      <h3>Add Student</h3>
-      <form onSubmit={handleSubmit} noValidate>
-          <div className='row'>
-            <div className='form-group'>
-            <label htmlFor='title'>Title (Uncontrolled) </label>
-              <input
-                type='text'
-                name='title'
-                ref={state.title}
-                onChange={handleChange}
-                placeholder='Title'
-              />
-
-              <label htmlFor='firstName'>First Name </label>
-              <input
-                type='text'
-                name='firstName'
-                value={state.firstName}
-                onChange={handleChange}
-                placeholder='First Name'
-                onBlur={handleBlur}
-              />
-              <br />
-              <small>{firstName}</small>
-            </div>
-            <div className='form-group'>
-              <label htmlFor='lastName'>Last Name </label>
-              <input
-                type='text'
-                name='lastName'
-                value={state.lastName}
-                onChange={handleChange}
-                placeholder='Last Name'
-              />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='email'>Email </label>
-              <input
-                type='email'
-                name='email'
-                value={state.email}
-                onChange={handleChange}
-                placeholder='Email'
-              />
-            </div>
-          </div>
-
-          <div className='form-group'>
-            <label htmlFor='tel'>Telephone </label>
-            <input
-              type='tel'
-              name='tel'
-              value={state.tel}
-              onChange={handleChange}
-              placeholder='Tel'
-            />
-          </div>
-
-          <div className='form-group'>
-            <label htmlFor='dateOfBirth'>Date of birth </label>
-            <input
-              type='date'
-              name='dateOfBirth'
-              value={state.dateOfBirth}
-              onChange={handleChange}
-              placeholder='Date of Birth'
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='favoriteColor'>Favorite Color</label>
-            <input
-              type='color'
-              id='color'
-              name='color'
-              value={state.favoriteColor}
-              onChange={handleChange}
-              placeholder='Favorite Color'
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='weight'>Weight </label>
-            <input
-              type='number'
-              id='weight'
-              name='weight'
-              value={state.weight}
-              onChange={handleChange}
-              placeholder='Weight in Kg'
-            />
-          </div>
-          <div>
-            <label htmlFor='country'>Country</label> <br />
-            <select name='country' onChange={handleChange} id='country'>
-              {selectCountries}
-            </select>
-          </div>
-
-          <div>
-            <p>Gender</p>
-            <div>
-              <input
-                type='radio'
-                id='female'
-                name='gender'
-                value='Female'
-                onChange={handleChange}
-                checked={state.gender === 'Female'}
-              />
-              <label htmlFor='female'>Female</label>
-            </div>
-            <div>
-              <input
-                id='male'
-                type='radio'
-                name='gender'
-                value='Male'
-                onChange={handleChange}
-                checked={state.gender === 'Male'}
-              />
-              <label htmlFor='male'>Male</label>
-            </div>
-            <div>
-              <input
-                id='other'
-                type='radio'
-                name='gender'
-                value='Other'
-                onChange={handleChange}
-                checked={state.gender === 'Other'}
-              />
-              <label htmlFor='other'>Other</label>
-            </div>
-          </div>
-
-          <div>
-            <p>Select your skills</p>
-            <div>
-              <input
-                type='checkbox'
-                id='html'
-                name='html'
-                onChange={handleChange}
-              />
-              <label htmlFor='html'>HTML</label>
-            </div>
-            <div>
-              <input
-                type='checkbox'
-                id='css'
-                name='css'
-                onChange={handleChange}
-              />
-              <label htmlFor='css'>CSS</label>
-            </div>
-            <div>
-              <input
-                type='checkbox'
-                id='javascript'
-                name='javascript'
-                onChange={handleChange}
-              />
-              <label htmlFor='javascript'>JavaScript</label>
-            </div>
-          </div>
-          <div>
-            <label htmlFor='bio'>Bio</label> <br />
-            <textarea
-              id='bio'
-              name='bio'
-              value={state.bio}
-              onChange={handleChange}
-              cols='120'
-              rows='10'
-              placeholder='Write about yourself ...'
-            />
-          </div>
-
-          <div>
-            <input type='file' name='file' onChange={handleChange} />
-          </div>
-          <div>
-            <button>Submit</button>
-          </div>
-        </form>
-    </div>
-  )
+  render() {
+    console.log('I am render and I will be the third to run.')
+    return (
+      <div className='App'>
+        <h1>React Component Life Cycle</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
