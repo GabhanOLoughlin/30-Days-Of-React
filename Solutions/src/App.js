7f78dd99fe43822c076aefa94079dbc8adafab45
Gabhan <gabhan@gabhan.com>
2023-06-21 19:30:45 +0100

    Complete button sample

--- Solutions/src/App.js
+++ Solutions/src/App.js
@@ -10,29 +10,86 @@ const Button = ({ onClick, text, style }) => {
   )
 }
 
-const buttonWithStyle = (CompParam) => {
+const buttonWithStyles = (CompParam, name = 'default') => {
+  const colors = [
+    {
+      name: 'default',
+      backgroundColor: '#e7e7e7',
+      color: '#000000',
+    },
+    {
+      name: 'react',
+      backgroundColor: '#61dbfb',
+      color: '#ffffff',
+    },
+    {
+      name: 'success',
+      backgroundColor: '#4CAF50',
+      color: '#ffffff',
+    },
+    {
+      name: 'info',
+      backgroundColor: '#2196F3',
+      color: '#ffffff',
+    },
+    {
+      name: 'warning',
+      backgroundColor: '#ff9800',
+      color: '#ffffff',
+    },
+    {
+      name: 'danger',
+      backgroundColor: '#f44336',
+      color: '#ffffff',
+    },
+  ]
+  const { backgroundColor, color } = colors.find((c) => c.name === name)
+
   const buttonStyles = {
-    backgroundColor: '#61dbfb',
-    padding: '10px 25px',
+    backgroundColor,
+    padding: '10px 45px',
     border: 'none',
-    borderRadius: 5,
+    borderRadius: 3,
     margin: 3,
     cursor: 'pointer',
-    fontSize: 18,
-    color: 'white',
+    fontSize: '1.25rem',
+    color,
   }
   return (props) => {
     return <CompParam {...props} style={buttonStyles} />
   }
 }
-const NewButton = buttonWithStyle(Button)
+
+const NewButton = buttonWithStyles(Button)
+const ReactButton = buttonWithStyles(Button, 'react')
+const InfoButton = buttonWithStyles(Button, 'info')
+const SuccessButton = buttonWithStyles(Button, 'success')
+const WarningButton = buttonWithStyles(Button, 'warning')
+const DangerButton = buttonWithStyles(Button, 'danger')
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Button text='No Style' />
-        <NewButton text='Styled Button' />
+        <Button text='No Style' onClick={() => alert('I am not styled yet')} />
+        <NewButton
+          text='Styled Button'
+          onClick={() => alert('I am the default style')}
+        />
+        <ReactButton text='React' onClick={() => alert('I have react color')} />
+        <InfoButton
+          text='Info'
+          onClick={() => alert('I am styled with info color')}
+        />
+        <SuccessButton text='Success' onClick={() => alert('I am successful')} />
+        <WarningButton
+          text='Warning'
+          onClick={() => alert('I\'ve warned you many times')}
+        />
+        <DangerButton
+          text='Danger'
+          onClick={() => alert('Oh no, you can not restore it')}
+        />
       </div>
     )
   }
