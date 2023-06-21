accebfc56cc07e7b157fcb7ec815b149286a4b32
Gabhan <gabhan@gabhan.com>
2023-06-21 23:05:53 +0100

    Country demo using useEffect

--- Solutions/src/App.js
+++ Solutions/src/App.js
@@ -1,222 +1,58 @@
 import './index.css';
 
 // index.js
-import React, { useState } from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 
-import gabhanImage from './images/Gabhan.jpg';
-
-//Function to show month date year
-const showDate = (time) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-
-  const month = months[time.getMonth()].slice(0, 3)
-  const year = time.getFullYear()
-  const date = time.getDate()
-  return ` ${month} ${date}, ${year}`
-}
-
-// User Card Component
-const UserCard = ({ user: { firstName, lastName, image } }) => (
-  <div className='user-card'>
-    <img src={image} alt={firstName} />
-    <h2>
-      {firstName}
-      {lastName}
-    </h2>
-  </div>
-)
-
-// A button component
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-)
-
-// CSS styles in JavaScript Object
-const buttonStyles = {
-  backgroundColor: '#61dbfb',
-  padding: 10,
-  border: 'none',
-  borderRadius: 5,
-  margin: 3,
-  cursor: 'pointer',
-  fontSize: 18,
-  color: 'white',
-}
-
-const Header = (props) => {
-  const {
-    welcome,
-    title,
-    subtitle,
-    author: { firstName, lastName },
-    date,
-  } = props.data
-
+const Country = ({ country: { name, flags, population } }) => {
   return (
-    <header style={props.styles}>
-      <div className='header-wrapper'>
-        <h1>{welcome}</h1>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flags.png} alt={name.common} />
+      </div>
+      <h3 className='country_name'>{name.common.toUpperCase()}</h3>
+      <div class='country_text'>
         <p>
-          {firstName} {lastName}
+          <span>Population: </span>
+          {population}
         </p>
-        <small>{date}</small>
       </div>
-    </header>
-  )
-}
-
-const Count = ({ count, addOne, minusOne }) => (
-  <div>
-    <h1>{count} </h1>
-    <div>
-      <Button text='+1' onClick={addOne} style={buttonStyles} />
-      <Button text='-1' onClick={minusOne} style={buttonStyles} />
     </div>
-  </div>
-)
-
-// TechList Component
-const TechList = (props) => {
-  const { techs } = props
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-  return techsFormatted
-}
-
-// Main Component
-const Main = (props) => {
-  const {
-    techs,
-    user,
-    greetPeople,
-    handleTime,
-    changeBackground,
-    count,
-    addOne,
-    minusOne,
-  } = props
-  return (
-    <main>
-      <div className='main-wrapper'>
-        <p>Prerequisite to get started react.js:</p>
-        <ul>
-          <TechList techs={techs} />
-        </ul>
-        <UserCard user={user} />
-        <Button
-          text='Greet People'
-          onClick={greetPeople}
-          style={buttonStyles}
-        />
-        <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
-        <Button
-          text='Change Background'
-          onClick={changeBackground}
-          style={buttonStyles}
-        />
-        <Count count={count} addOne={addOne} minusOne={minusOne} />
-      </div>
-    </main>
-  )
-}
-
-// Footer Component
-const Footer = (props) => {
-  return (
-    <footer>
-      <div className='footer-wrapper'>
-        <p>Copyright {props.date.getFullYear()}</p>
-      </div>
-    </footer>
   )
 }
 
 const App = (props) => {
-  const [count, setCount] = useState(0)
-  const [backgroundColor, setBackgroundColor] = useState('')
+  const [data, setData] = useState([])
 
-  const showDate = (time) => {
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-    const month = months[time.getMonth()].slice(0, 3)
-    const year = time.getFullYear()
-    const date = time.getDate()
-    return ` ${month} ${date}, ${year}`
+  const fetchData = async () => {
+    const url = 'https://restcountries.com/v3.1/all'
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setData(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  const addOne = () => {
-    setCount(count + 1)
-  }
-
-  // method which subtract one to the state
-  const minusOne = () => {
-    setCount(count - 1)
-  }
-  const handleTime = () => {
-    alert(showDate(new Date()))
-  }
-  const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2020')
-  }
-  const changeBackground = () => {}
-
-  const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Getting Started React',
-    subtitle: 'JavaScript Library',
-    author: {
-      firstName: 'Gabhan',
-      lastName: 'OLoughlin',
-    },
-    date: 'Oct 7, 2020',
-  }
-  const techs = ['HTML', 'CSS', 'JavaScript']
-
-  const user = { ...data.author, image: gabhanImage }
 
   return (
-    <div className='app'>
-      {backgroundColor}
-      <Header data={data} />
-      <Main
-        user={user}
-        techs={techs}
-        handleTime={handleTime}
-        greetPeople={greetPeople}
-        changeBackground={changeBackground}
-        addOne={addOne}
-        minusOne={minusOne}
-        count={count}
-      />
-      <Footer date={new Date()} />
+    <div className='App'>
+      <h1>Fetching Data Using Hook</h1>
+      <h1>Calling API</h1>
+      <div>
+        <p>There are {data.length} countries in the api</p>
+        <div className='countries-wrapper'>
+          {data.map((country) => (
+            <Country country={country} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
