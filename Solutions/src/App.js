8eefad0a651b087ccbc79a72bf25f5e4601f4df4
Gabhan <gabhan@gabhan.com>
2023-06-21 19:27:51 +0100

    Higher Order Button (inheritence)

--- Solutions/src/App.js
+++ Solutions/src/App.js
@@ -2,57 +2,37 @@ import './App.css';
 
 import React, { Component } from 'react';
 
-import axios from 'axios';
+const Button = ({ onClick, text, style }) => {
+  return (
+    <button onClick={onClick} style={style}>
+      {text}
+    </button>
+  )
+}
+
+const buttonWithStyle = (CompParam) => {
+  const buttonStyles = {
+    backgroundColor: '#61dbfb',
+    padding: '10px 25px',
+    border: 'none',
+    borderRadius: 5,
+    margin: 3,
+    cursor: 'pointer',
+    fontSize: 18,
+    color: 'white',
+  }
+  return (props) => {
+    return <CompParam {...props} style={buttonStyles} />
+  }
+}
+const NewButton = buttonWithStyle(Button)
 
 class App extends Component {
-  state = {
-    data: [],
-  }
-  componentDidMount() {
-    const API_URL = 'https://restcountries.com/v3.1/all'
-    axios
-      .get(API_URL)
-      .then((response) => {
-        this.setState({
-          data: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  renderCountries = () => {
-    return this.state.data.map((country) => {        
-      const formatLanguages = country.languages ? Object.keys(country.languages).join(', ') : ""   
-      const lengthOfLanguages = country.languages ? Object.keys(country.languages).length : -1;
-      const languageOrLanguages = lengthOfLanguages > 1 ? 'Languages' : 'Language'
-      return (
-        <div key={country.name.common}>
-          <div>
-            {' '}
-            <img src={country.flags.png} alt={country.name.common} />{' '}
-          </div>
-          <div>
-            <h1>{country.name.common}</h1>
-            <p>Population: {country.population}</p>
-            <p>
-              {languageOrLanguages} : {formatLanguages}
-            </p>
-            <p>Capital: {country.capital}</p>
-          </div>
-        </div>
-      )
-    })
-  }
   render() {
     return (
       <div className='App'>
-        <h1>Fetching Data Using Axios</h1>
-        <div>
-          <p>There are {this.state.data.length} countries in the api</p>
-          <div className='countries-wrapper'>{this.renderCountries()}</div>
-        </div>
+        <Button text='No Style' />
+        <NewButton text='Styled Button' />
       </div>
     )
   }
